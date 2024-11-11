57c6c13616cc63ec64348ae23d746d8f74054b96
vamshi25p <vamshikrishna788sai@gmail.com>
2024-11-11 13:31:16 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -1,22 +1,26 @@
 class Solution:
-   
+
     def getTotalIsles(self, grid: list[list[str]]) -> int:
-    #    write your code here
+        #    write your code here
         if not grid:
             return 0
         rows = len(grid)
         cols = len(grid[0])
-        visited=set()
-        def dfs(r,c):
-            if(r<0 or c<0 or r>=rows or c>=cols or grid[r][c]=='W' or (r,c) in visited):
+        visited = set()
+
+        def dfs(r, c):
+            if (r < 0 or c < 0 or r >= rows or c >= cols or grid[r][c] == 'W' or (r, c) in visited):
                 return
-            visited.add((r,c))
-            dfs(r+1,c)
-            dfs(r-1,c)
-            dfs(r,c+1)
-            dfs(r,c-1)
-            
-        count=0 
-        
-                    
-        return 0
+            visited.add((r, c))
+            dfs(r+1, c)
+            dfs(r-1, c)
+            dfs(r, c+1)
+            dfs(r, c-1)
+
+        count = 0
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == 'L' and (i, j) not in visited:
+                    dfs(i, j)
+                    count += 1
+        return count
