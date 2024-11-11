c49fb1afbdc44e5ba1b44919926b552a59b8d0e5
vamshi25p <vamshikrishna788sai@gmail.com>
2024-11-11 13:32:35 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -6,6 +6,7 @@ def decode_message( s: str, p: str) -> bool:
         dp[0][0]=True
         
         for i in range(1,n+1):
-                
+                if p[i-1]=='*':
+                        dp[0][i]=dp[0][i-1]
   
         return False
